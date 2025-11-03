b039b4597656ef4897a573453dd54f16ef90d8dd
Attreya <attreya01@gmail.com>
2025-11-03 12:37:35 +0530

    UI changes home + drive direction

--- src/drills/views.py
+++ src/drills/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Country
-from django.http import JsonResponse
 import random
 
-# Create your views here.
 def driving_direction(request):
     lhs_countries = list(Country.objects.filter(drive_side='LHS'))
     rhs_countries = list(Country.objects.filter(drive_side='RHS'))
     
+    # Check if there are any countries at all
+    if not lhs_countries and not rhs_countries:
+        # No countries in database - show error message
+        return HttpResponse(
+            '<h1>No countries found</h1>'
+            '<p>Please add countries to the database through the admin panel.</p>'
+            '<a href="/admin/">Go to Admin</a>',
+            status=404
+        )
+    
+    # Select a random country
     if lhs_countries and rhs_countries:
-        # Randomly choose LHS or RHS with equal probability
+        # Both types exist - randomly choose LHS or RHS with equal probability
         if random.choice([True, False]):
             country = random.choice(lhs_countries)
         else:
@@ -18,7 +28,7 @@ def driving_direction(request):
         # Only LHS countries exist
         country = random.choice(lhs_countries)
     else:
-        # Only RHS countries exist
+        # Only RHS countries exist (we know this list is not empty due to first check)
         country = random.choice(rhs_countries)
         
     context = {
